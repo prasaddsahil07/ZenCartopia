7c205b38de6d67b56c61a7f53987344b23534085
Sahil Prasad <sahilprasad41969@gmailcom>
2025-03-22 19:18:14 +0530

    environment variables included

--- backend/index.js
+++ backend/index.js
@@ -1,13 +1,17 @@
+import cookieParser from "cookie-parser";
 import express from "express";
-
 const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser());
 
 app.get("/", (req, res) => {
     console.log("Hello from ZenCartopia Server...");
     res.send("Welcome to ZenCartopia!");
 });
 
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
     console.log(`Server running on http://localhost:${PORT}`);
 });
